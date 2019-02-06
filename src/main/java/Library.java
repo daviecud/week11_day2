e8d43f65d68ab64fa346d7db5228ed56f87892f6
Dsvid Calderwood <davidwcaldewood@hotmail.com>
2019-02-06 08:54:55 +0000

    Refactoring

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,41 +1,5 @@
 import java.util.ArrayList;
 
-//
-//public class Library {
-//
-//    private int stock;
-//    private ArrayList<Book> books;
-//
-//    public Library(int stock) {
-//        this.stock = stock;
-//        this.books = new ArrayList<Book>();
-//    }
-//
-//    public ArrayList<Book> getBooks() {
-//        return books;
-//    }
-//
-//    public int checkStockLevel() {
-//        return stock;
-//    }
-//
-//    public int bookCount() {
-//        return this.books.size();
-//    }
-//
-//    public void addBook(Book book) {
-//        this.books.add();
-//    }
-//}
-////    public int addBookIfSpaceInStock(Book book) {
-////        if (this.bookCount() < this.stock) {
-////            this.books.add(book);
-////        }
-//
-
-
-
-
 public class Library {
     private int stock;
     private ArrayList<Book> books;
